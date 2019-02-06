2660d5366257c7e1ebc73279e677729821057149
srikii <44855671+srikii@users.noreply.github.com>
2019-02-05 20:53:29 -0500

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -5,6 +5,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 @author: jrr
 @author: rk
+@author: srikanth
 """
 
 import unittest
@@ -25,8 +26,28 @@ class TestTriangles(unittest.TestCase):
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    
+    def testTriA(self):
+        self.assertEqual(classifyTriangle(5,5,5),'Equilateral','5,5,5 is equilateral')
+
+    def testTriB(self):
+        self.assertEqual(classifyTriangle(3,3,4),'Isoceles','3,3,4, is a Isoceles triangle')
+    
+    def testTriC(self):
+        self.assertEqual(classifyTriangle(5,6,7),'Scalene', '5,6,7 is a scalene triangle')
+    
+    def testTriD(self):
+        self.assertEqual(classifyTriangle(5,12,13),'Right','5,12,13 is a right triangle')
+    
+    def testTriE(self):
+        self.assertEqual(classifyTriangle(10,2,3),'NotATriangle','10,2,3 is not a triangle')
+    
+    def testTriF(self):
+        self.assertEqual(classifyTriangle(5,5,5.5),'InvalidInput','5.5, 5.5, 5.5 is invalid as it takes only int and not float')
+
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
+    
+    
